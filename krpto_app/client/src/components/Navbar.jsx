56a8be5705fc417a52cfa562c49b34f984d55c06
Jaifan <jiku1213@gmail.com>
2022-05-18 23:19:11 +0600

    Card & Input place implement

--- krpto_app/client/src/components/Navbar.jsx
+++ krpto_app/client/src/components/Navbar.jsx
@@ -46,7 +46,6 @@ const Navbar = () => {
             </ul>
           )
 
-          
           }
 
 
--- krpto_app/client/src/components/Welcome.jsx
+++ krpto_app/client/src/components/Welcome.jsx
@@ -1,8 +1,79 @@
 
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  />
+);
+
 const Welcome = () => {
+
+  const handleSubmit = (e) => {
+    const { addressTo, amount, keyword, message } = formData;
+
+    e.preventDefault();
+
+    if (!addressTo || !amount || !keyword || !message) return;
+
+    sendTransaction();
+  };
+
+
   return (
-    <div>Welcome</div>
+    <div className="flex flex-col items-center bg-gradient-to-r from-green-400 to-blue-500">
+
+      <div className="">
+        <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card bg-gradient-to-r from-pink-500 to-yellow-500 ">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm">
+                  0000..0000
+                </p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+              </div>
+            </div>
+      </div>
+    </div>
+
+
+    <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center bg-black mb-6 rounded-lg">
+        <Input placeholder="Address To" name="addressTo" type="text" handleChange={()=> {}}  />
+        <Input placeholder="Amount (ETH)" name="amount" type="number"  handleChange={()=> {}}/>
+        <Input placeholder="Keyword (Gif)" name="keyword" type="text"  handleChange={()=> {}} />
+        <Input placeholder="Enter Message" name="message" type="text"  handleChange={()=> {}} />
+
+        <div className="h-[1px] w-full bg-gray-400 my-2" />
+
+ 
+      <button
+      type="button"
+      onClick={handleSubmit}
+      className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+      >
+        Send now
+      </button>
+              
+    </div>
+  </div>
+
+
   )
 }
 
-export default Welcome
+export default Welcome;
